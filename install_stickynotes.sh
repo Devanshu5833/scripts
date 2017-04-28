427c7e83117a4d6a273430aade6485587e88aa01
devanshu <devanshu5833@gmail.com>
2017-04-28 09:52:45 +0530

    ADDED : new script for sticky notes

---
+++ install_stickynotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing Start"
+echo "****************"
+sudo add-apt-repository ppa:umang/indicator-stickynotes
+sudo apt-get update
+sudo apt-get install indicator-stickynotes
+echo "**********"
+echo "completed"
+
